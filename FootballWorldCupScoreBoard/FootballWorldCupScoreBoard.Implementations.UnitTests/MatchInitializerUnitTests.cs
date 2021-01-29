d1e626567f0b58cd9e38d628ab11136e58dcee41
Yunier Tirado <ytirado@educaweb.com>
2021-01-29 12:26:21 +0100

    Adds UnitTests structure classes

---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard.Implementations.UnitTests/MatchInitializerUnitTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FootballWorldCupScoreBoard.Implementations.UnitTests
+{
+    [Trait("Type", "Unit")]
+    public class MatchInitializerUnitTests
+    {
+
+    }
+}
